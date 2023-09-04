15f89342711cc21bb30f51e78d8cb6bb194b51d2
jeanpierre89 <1jeanpierre.03@gmail.com>
2023-09-04 15:51:27 -0300

    Ficha Cadastro

---
+++ fiicha-cadastro/resources/views/ficha.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficha de Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro de Pessoa </h1>
+    <form action="" method="get" >
+        Nome1:     <input type="text" name="nome"><br>
+        Sobrenome:<input type="text" name="sobrenome"><br>
+        Endereço: <input type="text" name="endereco">
+        E-mail     <input type="text" name="email">
+        CPF :      <input type="number" name="cpf">
+        <input type="submit" name="Enviar">
+        
+    </form>
+    
+</body>
+</html>
